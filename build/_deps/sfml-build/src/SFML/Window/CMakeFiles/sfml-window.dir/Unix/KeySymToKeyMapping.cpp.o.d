75dad41dee890210f0b6a300043e4f4c97d7d919
g1ack52 <gg.galbiatigiacomo@gmail.com>
2026-01-02 15:47:17 +0100

    Rimozione venv e build dal repository

--- build/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o.d
+++
@@ -1,9 +0,0 @@
-_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o: \
- /home/ggal/workspace/projects/MDP_SC/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.cpp \
- /usr/include/stdc-predef.h \
- /home/ggal/workspace/projects/MDP_SC/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.hpp \
- /home/ggal/workspace/projects/MDP_SC/build/_deps/sfml-src/include/SFML/Window/Keyboard.hpp \
- /home/ggal/workspace/projects/MDP_SC/build/_deps/sfml-src/include/SFML/Window/Export.hpp \
- /home/ggal/workspace/projects/MDP_SC/build/_deps/sfml-src/include/SFML/Config.hpp \
- /usr/include/X11/X.h /usr/include/X11/keysym.h \
- /usr/include/X11/keysymdef.h
